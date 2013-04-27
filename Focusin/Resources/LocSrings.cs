c5dd271288f222dab18f20f6c9ce38a2a20215be
Frédéric Harper <fharper@oocz.net>
2013-04-27 10:11:56 -0400

    Fixed the upgrade from VS 2010 to 2012
    The app is now running, but the text doesn't show up anymore (will fix
    after). Thanks to Sebastien Lachance for the fix.

---
+++ Focusin/Resources/LocSrings.cs
@@ -0,0 +1,20 @@
+﻿namespace Focusin.Resources
+{
+    public class LocStrings
+    {
+        #region Fields
+
+        private static readonly Strings _resources = new Strings();
+
+        #endregion
+
+        #region Properties
+
+        public Strings Resources
+        {
+            get { return _resources; }
+        }
+
+        #endregion
+    }
+}
